5b6e08958b8871af04e3a5efd4e8dea597d7dced
BrenoLuna0 <breno.luna_@hotmail.com>
2020-09-09 16:07:40 -0300

    Teste de objeto chave valor

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,24 @@ routes.get("/", (req, res) => {
 });
 
 routes.post("/boleto", (req, res) => {
-  console.log(req.body);
+  const object = {
+    notifica_registrou: () => {
+      console.log("Seu Sorriso é ");
+    },
+    notifica_liquidou: () => {
+      console.log("tão resplandecente ");
+    },
+    notifica_baixou: () => {
+      console.log("que deixou meu coração alegre ");
+    },
+    notifica_rejeitou: () => {
+      console.log("Me dê a mão ");
+    },
+    notifica_alterou: () => {
+      console.log("pra fugir dessa terrível escuridão");
+    },
+  };
+  object[req.body.tipoWH]();
   return res.status(200).send(req.body);
 });
 
